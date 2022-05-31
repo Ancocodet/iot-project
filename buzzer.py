95c03e40265b5b8ada3ce40b3690575e0a42cf31
dseydel <dseydel@united-internet.de>
2022-05-31 14:41:03 +0200

    ⚡️

--- buzzer.py
+++ buzzer.py
@@ -8,10 +8,10 @@ GPIO.setup(buzzer,GPIO.OUT)
 
 try:
     while True:
-GPIO.output(buzzer,GPIO.HIGH)
-time.sleep(0.5)
-GPIO.output(buzzer,GPIO.LOW)
-time.sleep(0.5)
+        GPIO.output(buzzer,GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(buzzer,GPIO.LOW)
+        time.sleep(0.5)
 except KeyboardInterrupt:
     GPIO.cleanup()
 pass
