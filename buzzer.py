c0e8d0eb0304cbbbc33b934ceab0635703e2df89
Ancoplays <ancoplays@gmail.com>
2022-06-02 10:11:15 +0200

    Update files and add node-red-flow

--- buzzer.py
+++ buzzer.py
@@ -3,15 +3,15 @@ import time
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
-buzzer=4
-GPIO.setup(buzzer,GPIO.OUT)
+buzzer=7
 
+GPIO.setup(buzzer,GPIO.OUT)
 try:
     while True:
-        GPIO.output(buzzer,GPIO.HIGH)
+        GPIO.output(buzzer,True)
         time.sleep(0.5)
-        GPIO.output(buzzer,GPIO.LOW)
+        GPIO.output(buzzer,False)
         time.sleep(0.5)
 except KeyboardInterrupt:
     GPIO.cleanup()
-pass
+    pass
